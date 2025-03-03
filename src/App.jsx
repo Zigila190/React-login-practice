ddb52bd1b37322d27693097275d4cc8e1eea5fa8
zigila190 <egeonumatthewc@gmail.com>
2025-03-03 02:54:59 +0100

    corrected routes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import  React from 'react';
-import {BrowserRouter as Router, Route,Routes, Navigate} from 'react-router-dom';
-import LandingPage from './components/landingpage/LandigPage';
-import Register from './components/registration/register';
-import ForgotPassword from './components/forgotpassword/ForgotPassword';
+import {Route,Routes} from 'react-router-dom';
+import Register from './components/Register';
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
   return (
-    <Router>
+    
 
     <Routes>
-      <Route path = "/" element={<LandingPage />} />
-      <Route path = "/Registration" element={<Register />} />
+      <Route path = "/" element={<Register />} />
       <Route path = "/ForgotPassword" element={<ForgotPassword />} />
-      <Route path = "*" element={<Navigate />} />
       </Routes>
-      </Router>
+    
   );
 }
 
